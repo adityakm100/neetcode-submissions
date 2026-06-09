b84517479d88c032eb4d2827f64114193e0357fc
Aditya Kanginaya Madhuchandra <adityakm@umich.edu>
2026-06-09 13:58:31 -0400

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l = 0
+        r = 1 #not at the end inwards, we want to check the nearest possible combination closest to left and expand r outwards while its still working, so initialization of right is one more than left
+        maxProfit = 0
+
+        while r < len(prices): #left pointer shouldn't matter because left pointer only gets updated towards the right pointer on the failed iteration, right pointer is the one that keeps moving outwards so that is the only condition being checked, left pointer just gets moved to a position that right pointer has always checked
+            if prices[l] < prices[r]: #only calculate profit if there is a clear profit since we can always return 0 if there is no good profitable transaction
+                p = prices[r] - prices[l] #temp profit value
+                maxProfit = max(maxProfit, p) #update max profit
+            else:
+                l = r #we want the left pointer to be at the absolute minimum that it can possibly be, so through our algorithm, left will always inherently be placed at a local minimum. If the global minimum is at the end, there is no effect on calculations since there are computations done with a local minimum, but we wouldn't just increment it by 1
+            r += 1 #we always iterate right no matter what since if we update left to be at right, right would be one to the right which is what we want, otherwise, if the price of right was still higher meaning better sell, we iterate right to try to find a new potential better sell
+        return maxProfit
